704ad4f90b8bd1e74ef63dcf683601c1438d56af
Jonathan Moreno <johnfrom209@gmail.com>
2022-12-01 23:45:06 -0800

    initial commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './components/main-page';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
