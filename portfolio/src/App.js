9d2cbfa3666c853cd52c713b09ae05c9f33ca47f
Jonathan Moreno <johnfrom209@gmail.com>
2022-12-03 00:26:59 -0800

    added custom colors. removed some un needed files

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-import Main from './components/main-page';
+import Main from './components/Main';
 
 function App() {
   return (
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,tx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'mainColor': '#066A9B',
+      'mColordd': '#0D5A7F',
+      'mColorll': '#279ED8',
+      'mColorl': '#098BCA',
+      'mColord': '#06628E',
+      'sColorMain': '#222B32',
+      'sColorll': '#35414B',
+      'sColorl': '#2D3C48',
+      'sColord': '#2C363F',
+      'sColordd': '#27323B'
+    }
   },
   plugins: [],
 }
