290cc5a66a08332bf6ec817e4652f552dfbbeb27
Jonathan Moreno <johnfrom209@gmail.com>
2022-12-03 17:32:51 -0800

    added a background to the page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,32 @@
+import React, { Component } from 'react';
 import Main from './components/Main';
+import background from "./images/polygon.png"
 
-function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+// function App() {
+
+
+//   return (
+//     <div className="App">
+//       <Main />
+//     </div>
+//   );
+// }
+
+
+class App extends Component {
+  render() {
+    const myStyle = {
+      backgroundImage: `url(${background})`,
+      height: '100vh',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+    };
+    return (
+      <div style={myStyle}>
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
