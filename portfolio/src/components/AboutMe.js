0960358c0ada16a232c577b750b2e83dedcd9e1b
Jonathan Moreno <johnfrom209@gmail.com>
2022-12-05 01:27:35 -0800

    Made a new aboutme paragraph

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -5,14 +5,12 @@ function AboutMe() {
 
     return (
 
-        <div className="mx-56 mt-40 font-bold">
+        <div className="mx-56 mt-40 font-bold bg-sColorll rounded-xl p-5 shadow-xl ">
             <h4 className="text-5xl text-mColord  mb-5 flex">AboutMe</h4>
             <img className="max-h-56 flex rounded-full border-4 border-mColord" src={selfie} alt="Selfie of Jonathan Moreno"></img>
-            <p className="text-2xl text-white leading-loose font-semibold"> I like to code. Recently I have been doing HTML and CSS at UCB coding bootcamp as I want to be a
-                Full Stack Developer.
-                While you are here why not check out my work below. I also experienced Java at Merced Community College and C# inside of Unity Engine. I
-                live in Central Valley California. During my off time I enjoy drawing as it acts as meditation, though
-                I'm still new to it all.</p>
+            <p className="text-2xl text-white leading-loose font-semibold">
+                I like to code. The feeling of creating something from nothing is a feeling that I can't get enough of. I am a full stack web developer with a background in self management. I am a graduate of the University of California, Berkeley Extension Coding Bootcamp. I am currently looking for a full time position as a web developer. I am proficient in many skills relating to web Development, a full list is on the Resume page. I am a fast learner and I am always looking to improve my skills. I am a team player and I am always looking to collaborate with others. I am a problem solver and I am always looking for new challenges. Each problem is like a puzzle and I enjoy delving into them. I am a hard worker and I am always looking to improve myself. I am a creative thinker and I am always looking to create something new. Learning web development has opened my mind to the endless possibilities of things I can create.
+            </p>
         </div >
 
     )
