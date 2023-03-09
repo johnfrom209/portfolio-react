44258fbb15242f8ae729291ff0cb3c4fb1f5352a
Jonathan Moreno <johnfrom209@gmail.com>
2023-03-08 17:24:24 -0800

    fixed how the page is viewed for mobile. Rewrote the About me Section

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -1,17 +1,17 @@
 import React from "react";
-import selfie from "../images/selfie.jpg";
+// import selfie from "../images/selfie.jpg";
 
 function AboutMe() {
 
     return (
 
-        <div className="mx-56 mt-10 font-bold bg-sColorll rounded-xl shadow-xl p-3">
+        <div className="xl:mx-56 mt-10 font-bold bg-sColorll rounded-xl shadow-xl p-3 border-4 border-mColord h-full mb-32">
             {/* <h4 className="text-2xl text-mColord  mb-5 flex">AboutMe</h4> */}
-            <img className="max-h-56 inline-block rounded-full border-4 border-mColord" src={selfie} alt="Selfie of Jonathan Moreno"></img>
-            <p className="text-2xl p-5 text-white leading-loose font-semibold inline-block">Hello, I'm Jonathan Moreno, <br></br>
+            {/* <img className="max-h-56 inline-block rounded-full border-4 border-mColord" src={selfie} alt="Selfie of Jonathan Moreno"></img> */}
+            <p className="text-2xl p-5 text-white leading-loose font-semibold inline-block">Greetings, my name is Jonathan Moreno, <br></br>
             </p>
             <p className="text-lg indent-20 text-white leading-loose font-semibold">
-                I like to code. The feeling of creating something from nothing is a feeling that I can't get enough of. I am a full stack web developer with a background in self management. I am a graduate of the University of California, Berkeley Extension Coding Bootcamp. I am currently looking for a full time position as a web developer. I am proficient in many skills relating to web Development, a full list is on the Resume page. I am a fast learner and I am always looking to improve my skills. I am a team player and I am always looking to collaborate with others. I am a problem solver and I am always looking for new challenges. Each problem is like a puzzle and I enjoy delving into them. I am a hard worker and I am always looking to improve myself. I am a creative thinker and I am always looking to create something new. Learning web development has opened my mind to the endless possibilities of things I can create.
+                and I have a passion for coding. The sense of satisfaction that comes with creating something from scratch is one that I constantly crave. As a full stack web developer with a background in self-management, I am a proud graduate of the University of California, Berkeley Extension Coding Bootcamp. Currently, I am seeking a full-time role as a web developer, and my expertise in various web development skills can be found on my Resume page. I am a quick learner who is dedicated to enhancing my abilities. Collaboration is key to me as I value teamwork and consider myself a team player. Problem-solving is another area in which I excel, and I enjoy tackling new challenges, treating each one like a unique puzzle. Hard work and self-improvement are my driving forces, and I am always looking for ways to be more creative and innovative in my approach to web development. My experience in web development has opened up a whole new world of possibilities, and I look forward to creating even more exciting projects in the future.
             </p>
         </div >
 
